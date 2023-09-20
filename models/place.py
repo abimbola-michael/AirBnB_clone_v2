ee1621fda021a15605c81b87fa23230f7fe99f3f
Aderemi <omoremilekun@gmail.com>
2023-09-20 09:09:11 -0700

    updating

--- models/place.py
+++ models/place.py
@@ -8,26 +8,27 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
-        Column('amenity_id', String(60),
-            ForeignKey('amenities.id'), nullable=False, primary_key=True))
+place_amenity = Table('place_amenity', Base.metadata, Column(
+    'place_id', String(60), ForeignKey('places.id'), primary_key=True), Column(
+        'amenity_id', String(60), ForeignKey(
+            'amenities.id'), nullable=False, primary_key=True))
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id =  Column(String(60), ForeignKey('users.id'), nullable=False)
-    name =  Column(String(128), nullable=False)
-    description =  Column(String(1024))
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+    amenities = relationship(
+            "Amenity", secondary="place_amenity", viewonly=False)
     reviews = relationship("Review", backref='place', cascade="all, delete")
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
