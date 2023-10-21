42b9b850ae2e544c3b6b91f6e1e6d53721644a02
Abimbola <abimbolamichael100@gmail.com>
2023-10-21 07:14:24 -0700

    Updata

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,13 +12,17 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """display a HTML page: (inside the tag BODY)"""
-    states = sorted(list(storage.all("State").values()), key=lambda val: val.name)
+    states = sorted(
+            list(storage.all("State").values()), key=lambda val: val.name
+            )
     return render_template("7-states_list.html", states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes storage on teardown"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
