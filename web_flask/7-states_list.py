d2cadc693b5ee02b17790704bbdda4ac41e027e5
Abimbola <abimbolamichael100@gmail.com>
2023-10-21 07:30:33 -0700

    Updata

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,8 +3,8 @@
 script that starts a Flask web application
 """
 
-from flask import Flask. render_template
-from models import *
+from flask import Flask, render_template
+from models.state import State
 from models import storage
 app = Flask(__name__)
 
@@ -13,16 +13,16 @@ app = Flask(__name__)
 def states_list():
     """display a HTML page: (inside the tag BODY)"""
     states = sorted(
-            list(storage.all("State").values()), key=lambda val: val.name
+            list(storage.all(State).values()), key=lambda val: val.name
             )
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exception):
     """closes storage on teardown"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
